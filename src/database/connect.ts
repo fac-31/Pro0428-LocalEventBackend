5cccd38997369943ba007385dc4414c078a68284
Richard Couzens <richardcouzens96@gmail.com>
2025-05-07 13:00:34 +0100

    chore(events): implement route/controller/service for GET /events

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -3,8 +3,14 @@ import { Db, MongoClient } from 'npm:mongodb@6.1.0';
 const dbConnString = Deno.env.get('DB_CONN_STRING');
 const dbName = Deno.env.get('DB_NAME');
 
-if (!dbConnString || !dbName) {
-  throw new Error('DB_CONN_STRING or DB_NAME environment variable(s) not set.');
+console.log(dbConnString);
+console.log(dbName);
+
+if (!dbConnString) {
+  throw new Error('DB_CONN_STRING environment variable not set.');
+}
+if (!dbName) {
+  throw new Error('DB_NAME environment variable not set.');
 }
 
 const client = new MongoClient(dbConnString);
--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,17 +1,12 @@
 import { Router } from '../../deps.ts';
+import { getAllEvents } from '../controllers/event.controller.ts';
 
 const router = new Router();
 
 // Routes under /events
 
-// Temporary stub for testing
-router
-  .get('/', (ctx) => {
-    ctx.response.body = 'events route root';
-  });
-
 // -> to controllers
-// router.get("/", getAllEvents)
+router.get("/", getAllEvents)
 // router.get("/:id", getEvent)
 // router.post("/")
 // router.put("/:id", updateEvent)
