988c567160c68492ceba9baa0e7e4c5ba345454d
arecouz <richardcouzens96@gmail.com>
2025-05-08 13:15:14 +0100

    removed console.logs

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -3,9 +3,6 @@ import { Db, MongoClient } from 'npm:mongodb@6.1.0';
 const dbConnString = Deno.env.get('DB_CONN_STRING');
 const dbName = Deno.env.get('DB_NAME');
 
-console.log(dbConnString);
-console.log(dbName);
-
 if (!dbConnString) {
   throw new Error('DB_CONN_STRING environment variable not set.');
 }
