b0dcdc1d215b1567727d947d5469d3e8a951dcf2
David Ogden <DavidOgden112@gmail.com>
2025-05-09 11:11:21 +0100

    OpenAi and Zod in deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export { Router } from 'https://deno.land/x/oak/mod.ts';
 export { Context } from 'https://deno.land/x/oak/mod.ts';
+export { OpenAI } from "https://deno.land/x/openai@v4.69.0/mod.ts";
+export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
