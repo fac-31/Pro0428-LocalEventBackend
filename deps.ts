99b3931852f686b9d6dbf129adf595124b808ae7
David Ogden <DavidOgden112@gmail.com>
2025-05-09 12:20:16 +0100

    Changed to Jsr method of install for OpenAI. Created Event Schema with Zod.

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
 export { Router } from 'https://deno.land/x/oak/mod.ts';
 export { Context } from 'https://deno.land/x/oak/mod.ts';
-export { OpenAI } from "https://deno.land/x/openai@v4.69.0/mod.ts";
 export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
