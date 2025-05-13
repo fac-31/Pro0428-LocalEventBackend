5005b80d83854fdd53aaf6ed1358affd126126b3
Richard Couzens <richardcouzens96@gmail.com>
2025-05-13 22:32:00 +0100

    deno import additions for auth

--- deps.ts
+++ deps.ts
@@ -1,3 +1,10 @@
 export { Context, Router, Status } from 'https://deno.land/x/oak/mod.ts';
-export type { RouterContext } from 'https://deno.land/x/oak/mod.ts';
+export {
+  create,
+  getNumericDate,
+  verify,
+} from 'https://deno.land/x/djwt/mod.ts';
 export { ObjectId } from 'npm:mongodb@6.1.0';
+
+// Types
+export type { Next, RouterContext } from 'https://deno.land/x/oak/mod.ts';
