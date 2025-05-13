71ce7b827f8fe4980d6e05ecb7bb80546fe6fe9c
Richard Couzens <richardcouzens96@gmail.com>
2025-05-13 22:30:27 +0100

    updated user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,29 @@
+import { z } from 'https://deno.land/x/zod/mod.ts';
 import { ObjectId } from '../../deps.ts';
 
-export interface User {
-  name_first: string;
-  name_last: string;
+export const UserInputSchema = z.object({
+  name_first: z.string().optional(),
+  name_last: z.string().optional(),
+  email: z.string().email(),
+  username: z.string().optional(),
+  password: z.string().min(6), // plain-text
+});
+
+export type UserInput = z.infer<typeof UserInputSchema>;
+
+export interface UserInDB {
+  _id?: ObjectId;
+  name_first?: string;
+  name_last?: string;
   email: string;
-  username: string;
-  password: string;
+  username?: string;
+  password: string; // hashed
   saved_events: ObjectId[];
 }
+
+export type SafeUser = Omit<UserInDB, 'password'>;
+
+export const toSafeUser = (user: UserInDB): SafeUser => {
+  const { password: _password, ...safeUser } = user;
+  return safeUser;
+};
