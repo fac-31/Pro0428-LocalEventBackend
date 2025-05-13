aeeb7c15ad0ec80b51eab8af4c1aa6d8181f7a20
Richard Couzens <richardcouzens96@gmail.com>
2025-05-13 22:33:21 +0100

    attatched loggin middleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import 'https://deno.land/std@0.224.0/dotenv/load.ts';
 import { Application } from 'https://deno.land/x/oak/mod.ts';
 import './database/connect.ts';
 import router from './routes/index.ts';
+import logRequest from './middleware/logRequest.ts';
 
 const app = new Application();
 
+app.use(logRequest);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
