88edc93b7d47b60880a201fa2eddb5ac6c15d85d
Richard Couzens <richardcouzens96@gmail.com>
2025-05-13 22:29:41 +0100

    generate token and verify token util functions

---
+++ src/utils/token.utils.ts
@@ -0,0 +1,29 @@
+import { create, verify } from '../../deps.ts';
+import { Payload } from 'https://deno.land/x/djwt@v3.0.2/mod.ts';
+
+const secret = Deno.env.get('CRYPTO_SECRET');
+if (!secret) {
+  throw new Error('CRYPTO_SECRET environment variable not set.');
+}
+
+const secretArray = new Uint8Array(
+  atob(secret)
+    .split('')
+    .map((char) => char.charCodeAt(0)),
+);
+
+const cryptoKey = await crypto.subtle.importKey(
+  'raw',
+  secretArray,
+  { name: 'HMAC', hash: 'SHA-256' },
+  false,
+  ['sign', 'verify'],
+);
+
+export const generateToken = async (payload: Payload) => {
+  return await create({ alg: 'HS256', typ: 'JWT' }, payload, cryptoKey);
+};
+
+export const verifyToken = async (token: string, key = cryptoKey) => {
+  return await verify(token, key);
+};
