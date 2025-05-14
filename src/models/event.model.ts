b29e0568b5a27f8b9586a1be01a36597fee41088
David Ogden <DavidOgden112@gmail.com>
2025-05-14 18:55:46 +0100

    Ran lint and fmt

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -1,25 +1,25 @@
-import { z } from '../../deps.ts'
+import { z } from '../../deps.ts';
 
 const eventModeEnum = z.enum(['Music', 'Charity', 'Sports', 'Other']);
 
 export type EventMode = z.infer<typeof eventModeEnum>;
 
 export const eventSchema = z.object({
-    mode: eventModeEnum,
-    name: z.string(),
-    description: z.string(),
-    location: z.string(),
-    date: z.string(),
-    price: z.number(),
-    url: z.string()   
+  mode: eventModeEnum,
+  name: z.string(),
+  description: z.string(),
+  location: z.string(),
+  date: z.string(),
+  price: z.number(),
+  url: z.string(),
 });
 
-export const eventsArraySchema =  z.object({
-    musicEvents: z.array(eventSchema),
-    charityEvents: z.array(eventSchema),
-    sportEvents: z.array(eventSchema),
-    otherEvents: z.array(eventSchema)
-})
+export const eventsArraySchema = z.object({
+  musicEvents: z.array(eventSchema),
+  charityEvents: z.array(eventSchema),
+  sportEvents: z.array(eventSchema),
+  otherEvents: z.array(eventSchema),
+});
 
 export type Event = z.infer<typeof eventSchema>;
 
--- src/utils/save.events.ts
+++ src/utils/save.events.ts
@@ -1,16 +1,16 @@
-import {Event, CompleteEventType } from '../models/event.model.ts';
+import { CompleteEventType, Event } from '../models/event.model.ts';
 
 import { db } from '../database/connect.ts';
 
 const eventCollection = db.collection<Event>('events');
 
 const databaseIncludes = async (event: Event): Promise<boolean> => {
-    const exists = await eventCollection.findOne({
-                name: event.name,
-                date: event.date,
-                location: event.location
-                });
-    return exists !== null;
+  const exists = await eventCollection.findOne({
+    name: event.name,
+    date: event.date,
+    location: event.location,
+  });
+  return exists !== null;
 };
 
 const isCompleteEventType = (obj: unknown): obj is CompleteEventType => {
@@ -22,24 +22,17 @@ const isCompleteEventType = (obj: unknown): obj is CompleteEventType => {
 };
 
 export const saveEvents = async (input: Event | CompleteEventType) => {
-
-    if (isCompleteEventType(input)) {
-        for (const eventCategory of Object.values(input)) {
-
-            for (const event of eventCategory) {
-
-                if (!(await databaseIncludes(event))) {
-
-                    await eventCollection.insertOne(event)
-                };
-            };
-                
-        };
-    } else {
-    
-        if (!(await databaseIncludes(input))) {
-
-            await eventCollection.insertOne(input);
-        };
-    };
+  if (isCompleteEventType(input)) {
+    for (const eventCategory of Object.values(input)) {
+      for (const event of eventCategory) {
+        if (!(await databaseIncludes(event))) {
+          await eventCollection.insertOne(event);
+        }
+      }
+    }
+  } else {
+    if (!(await databaseIncludes(input))) {
+      await eventCollection.insertOne(input);
+    }
+  }
 };
