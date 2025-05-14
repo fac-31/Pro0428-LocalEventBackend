f2bf993e0bab676375ad9e7ac8b2cc060beb2d7a
David Ogden <DavidOgden112@gmail.com>
2025-05-14 18:55:46 +0100

    Ran lint and fmt

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { Context, Router, Status } from 'https://deno.land/x/oak/mod.ts';
 export type { RouterContext } from 'https://deno.land/x/oak/mod.ts';
 export { ObjectId } from 'npm:mongodb@6.1.0';
-export { OpenAI } from '@openai/openai'
-export { z } from 'npm:zod@3'
+export { OpenAI } from '@openai/openai';
+export { z } from 'npm:zod@3';
 export { zodTextFormat } from '@openai/openai/helpers/zod';
--- src/utils/update.events.ts
+++ src/utils/update.events.ts
@@ -1,33 +1,33 @@
 import { saveEvents } from './save.events.ts';
 import { generateEvents } from '../services/openai.service.ts';
 
-
-// This file is useless until deployment. Have fun. 
+// This file is useless until deployment. Have fun.
 
 Deno.serve(async (req: Request) => {
-    const url = new URL(req.url);
-    const token = url.searchParams.get("token");
+  const url = new URL(req.url);
+  const token = url.searchParams.get('token');
 
-    // Change this to appropriate .env or github secret
-    const expectedToken = Deno.env.get("DAILY_JOB_TOKEN");
+  // Change this to appropriate .env or github secret
+  const expectedToken = Deno.env.get('DAILY_JOB_TOKEN');
 
-    if (token !== expectedToken) {
-        return new Response("Forbidden", {status: 403});
-    };
-    
-    console.log("Secure daily task triggered");
+  if (token !== expectedToken) {
+    return new Response('Forbidden', { status: 403 });
+  }
 
-    const events = await generateEvents(["Music", 'Charity', 'Sports', 'Other'], "Finsbury Park");
+  console.log('Secure daily task triggered');
 
-    if (events !== null) {
+  const events = await generateEvents(
+    ['Music', 'Charity', 'Sports', 'Other'],
+    'Finsbury Park',
+  );
 
+  if (events !== null) {
     await saveEvents(events);
 
-    console.log("Events saved");
-    } else {
+    console.log('Events saved');
+  } else {
+    console.log('Cannot save events. Events equalled null');
+  }
 
-        console.log("Cannot save events. Events equalled null")
-    }
-
-    return new Response("Daily task completed");
-})
+  return new Response('Daily task completed');
+});
