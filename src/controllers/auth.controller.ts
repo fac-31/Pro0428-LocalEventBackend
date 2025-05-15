11518cbbf658f59cea5ef592ab4479e07671eaa6
Richard Couzens <richardcouzens96@gmail.com>
2025-05-15 13:25:30 +0100

    changed numeric status codes to Oaks named Status for better readability

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file require-await
-import { Context, RouterContext } from '../../deps.ts';
+import { Context, RouterContext, Status } from '../../deps.ts';
 import { UserLogInSchema, UserSignUpSchema } from '../models/user.model.ts';
 import { authService } from '../services/auth.service.ts';
 
@@ -7,12 +7,12 @@ export const getCurrentUser = async (ctx: Context) => {
   const user = ctx.state.user;
 
   if (!user) {
-    ctx.response.status = 401;
+    ctx.response.status = Status.Unauthorized;
     ctx.response.body = { message: 'User not authenticated' };
     return;
   }
   ctx.response.body = {
-    message: `Hello, ${user.email}!`,
+    message: `Hello, ${user.username}!`,
     user,
   };
 };
@@ -22,16 +22,16 @@ export const signUpUser = async (ctx: RouterContext<'/signup'>) => {
   const userInput = UserSignUpSchema.safeParse(body);
 
   if (!userInput.success) {
-    ctx.response.status = 400;
+    ctx.response.status = Status.BadRequest;
     ctx.response.body = { errors: userInput.error.flatten() };
     return;
   }
   try {
     const insertedId = await authService.createUser(userInput.data);
-    ctx.response.status = 201;
+    ctx.response.status = Status.Created;
     ctx.response.body = insertedId;
   } catch (error) {
-    ctx.response.status = 500;
+    ctx.response.status = Status.InternalServerError;
     if (error instanceof Error) {
       ctx.response.body = { error: error.message };
     } else {
@@ -44,21 +44,22 @@ export const loginUser = async (ctx: RouterContext<'/login'>) => {
   const body = await ctx.request.body.json();
   const userInput = UserLogInSchema.safeParse(body);
   if (!userInput.success) {
-    ctx.response.status = 400;
+    ctx.response.status = Status.BadRequest;
     ctx.response.body = { errors: userInput.error.flatten() };
     console.error(userInput.error);
     return;
   }
   try {
     const token = await authService.logInUser(userInput.data);
-    ctx.response.status = 200;
+    ctx.response.status = Status.OK;
     ctx.response.body = { token };
   } catch (error) {
     if (error instanceof Error) {
-      ctx.response.status = 401;
+      ctx.response.status = Status.Unauthorized;
       ctx.response.body = { error: error.message };
       console.error(error.message);
     } else {
+      ctx.response.status = Status.InternalServerError;
       ctx.response.body = { error: 'Unkown error creating token' };
       console.error(error);
     }
