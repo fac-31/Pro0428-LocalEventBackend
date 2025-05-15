5f0a2e07c62180001c98b4d71c400872ee51a9a4
David Ogden <DavidOgden112@gmail.com>
2025-05-15 19:00:22 +0100

    Changed update events to use status instead of numbers. Updated the api examples to include username. Changed daily-job to use headers instead of params as this is more secure.

--- src/utils/update.events.ts
+++ src/utils/update.events.ts
@@ -1,20 +1,20 @@
 import { saveEvents } from './save.events.ts';
 import { generateEvents } from '../services/openai.service.ts';
+import { Status } from '../../deps.ts';
 
 // This file is useless until deployment. Have fun.
 
 Deno.serve(async (req: Request) => {
-  const url = new URL(req.url);
-  const token = url.searchParams.get('token');
+  const token = req.headers.get("X-Daily-Token");
 
   // Change this to appropriate .env or github secret
   const expectedToken = Deno.env.get('DAILY_JOB_TOKEN');
 
   if (token !== expectedToken) {
-    return new Response('Forbidden', { status: 403 });
+    return new Response("Forbidden", {status: Status.Forbidden} );
   }
 
-  console.log('Secure daily task triggered');
+  console.log('Daily task triggered');
 
   const events = await generateEvents(
     ['Music', 'Charity', 'Sports', 'Other'],
@@ -26,7 +26,8 @@ Deno.serve(async (req: Request) => {
 
     console.log('Events saved');
   } else {
-    console.log('Cannot save events. Events equalled null');
+    // We could implement a delete out of date event function around here 
+    console.log('Cannot save events; events was null');
   }
 
   return new Response('Daily task completed');
