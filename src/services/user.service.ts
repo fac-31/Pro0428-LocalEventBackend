4651148d7f3c2da613f156956e050a0841711473
David Ogden <DavidOgden112@gmail.com>
2025-06-12 15:30:19 +0100

    feat: :sparkles: Set up back end routes for user Events
    GetAllEvents now gets user events by mode as well and returns an pbject of all events and the object Ids of the saved user events. Those id's are then checked on the front end against the rest of the events. The handle user events handles the toggling of the save button.

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -6,20 +6,18 @@
 import 'https://deno.land/std@0.224.0/dotenv/load.ts';
 import { db } from '../database/connect.ts';
 import { UserInDB } from 'models/user.model.ts';
-import { hash, OptionalId } from '../../deps.ts';
+import { hash, ObjectId, OptionalId } from '../../deps.ts';
 import { NewUser } from 'models/user.model.ts';
+import { FullEvent } from '../../shared/src/models/event.model.ts';
 
 const users = db.collection<OptionalId<UserInDB>>('users');
+const events = db.collection<FullEvent>('events')
 
 const getAllUsers = async (
   role: 'user' | 'admin' | 'all',
 ): Promise<UserInDB[]> => {
   const query = role !== 'all' ? { role } : {};
   return await users.find(query).toArray();
-};
-
-export const userService = {
-  getAllUsers,
 };
 
 const _createAdmin = async () => {
@@ -37,3 +35,35 @@ const _createAdmin = async () => {
 
   console.log('Admin created:', result);
 };
+
+const handleUserEvents = async (eventId: string, userId: string, saving: boolean) => {
+  console.log(saving)
+  if (saving === true) {
+      await users.updateOne(
+       { _id: new ObjectId(userId) },
+       { $addToSet : { saved_events: new ObjectId(eventId) } }
+    )
+  } else {
+    await users.updateOne(
+      { _id: new ObjectId(userId) },
+      { $pull: { saved_events: new ObjectId(eventId) } }
+    )
+  }
+}
+
+const getUserEvents = async (userId: string) => {
+  const user = await users.findOne({_id: new ObjectId(userId) });
+  const savedEvents = user?.saved_events ?? [];
+  const eventsList = await events.find({ _id: { $in: savedEvents } }).toArray();
+  return eventsList;
+}
+
+export const userService = {
+  getAllUsers,
+  handleUserEvents,
+  getUserEvents
+};
+/*
+User clicks heart. Event target id? This is passed to backend via post request. Route grabs this. Calls this function via the 
+user controller. Save the id which should reference 
+*/
