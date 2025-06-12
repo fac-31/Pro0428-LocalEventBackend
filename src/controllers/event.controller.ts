08bfe8f5ffdb577f1c80b403c176245f557da4cf
David Ogden <DavidOgden112@gmail.com>
2025-06-12 17:21:41 +0100

    Ran check, fmt, lint

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -89,7 +89,7 @@ export const updateEventById = async (ctx: RouterContext<'/:id'>) => {
 
   const id: string = ctx.params.id;
   const event: Partial<FullEvent> = await ctx.request.body.json();
-console.log(event)
+  console.log(event);
   if (!ObjectId.isValid(id)) {
     ctx.response.status = Status.BadRequest;
     ctx.response.body = `Invalid event id "${id}"`;
