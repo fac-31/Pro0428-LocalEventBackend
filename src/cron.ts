5a8f8384fa004d483be5cc94a89f2f75c551ac5d
Jaz-spec <jaz.edna@proton.me>
2025-06-24 14:21:39 +0100

    disabled cron

--- src/cron.ts
+++ src/cron.ts
@@ -1,26 +1,26 @@
 import { eventService } from './services/event.service.ts';
 import { generateEvents } from './services/openai.service.ts';
 
-Deno.cron('save events cron', "*/10 * * * *", async () => {
-  console.log('executing cron job...');
-  try {
-    console.log('Daily task triggered');
+// Deno.cron('save events cron', "*/10 * * * *", async () => {
+//   console.log('executing cron job...');
+//   try {
+//     console.log('Daily task triggered');
 
-    const events = await generateEvents(
-      ['music', 'charity', 'sports', 'other'],
-      'Finsbury Park',
-    );
+//     const events = await generateEvents(
+//       ['music', 'charity', 'sports', 'other'],
+//       'Finsbury Park',
+//     );
 
-    if (events !== null) {
-      await eventService.saveEvents(events);
-      console.log('Events saved successfully');
-    } else {
-      console.log('There were no events to save');
-    }
-  } catch (error) {
-    console.error('Cron job failed:', error);
-  }
-});
+//     if (events !== null) {
+//       await eventService.saveEvents(events);
+//       console.log('Events saved successfully');
+//     } else {
+//       console.log('There were no events to save');
+//     }
+//   } catch (error) {
+//     console.error('Cron job failed:', error);
+//   }
+// });
 
 //cron for every 10mins
 // "*/3 * * * *"
